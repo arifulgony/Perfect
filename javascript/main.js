dcc034a8caa97f0e662adbea75b65840821916a6
arifulgony <arifulgony@gmail.com>
2023-08-10 12:51:07 +0600

    fooder

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,4 @@
+ //header
  function toggleMenu() {
             var toggle = document.querySelector(".toggle");
             var nav = document.querySelector(".nav");
@@ -5,3 +6,19 @@
             nav.classList.toggle("active");
         }
 
+//Accoding
+        const items = document.querySelectorAll(".accordion button");
+
+        function toggleAccordion() {
+            const itemToggle = this.getAttribute('aria-expanded');
+
+            items.forEach(item => {
+                item.setAttribute('aria-expanded', 'false');
+            })
+
+            if (itemToggle == 'false') {
+                this.setAttribute('aria-expanded', 'true');
+            }
+        }
+
+        items.forEach(item => item.addEventListener('click', toggleAccordion));
